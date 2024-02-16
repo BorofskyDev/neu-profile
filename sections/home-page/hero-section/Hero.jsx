ce9746b2e745237fcf52ee8236423020e1d4c707
solovyovsdom <joelborofsky@mac.com>
2024-02-16 17:02:19 -0600

    Update padding in SubTitleContainer and add HeroImage component

--- sections/home-page/hero-section/Hero.jsx
+++ sections/home-page/hero-section/Hero.jsx
@@ -1,10 +1,12 @@
 import styles from './Hero.module.scss'
 import HeroContent from './content/HeroContent'
+import HeroImage from './image/HeroImage'
 
 export default function Hero() {
     return (
         <section className={styles.hero}> 
         <HeroContent className='light-bg-blue' />
+        <HeroImage className='light-bg-green' />
         </section>
     )
 }
