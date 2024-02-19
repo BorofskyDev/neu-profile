25dfa52953efe2f219876fb92b0b3e87f44cfbe9
solovyovsdom <joelborofsky@mac.com>
2024-02-19 11:04:14 -0600

    Add position styles to generic container and image container

--- components/containers/image-container/ImageContainer.jsx
+++ components/containers/image-container/ImageContainer.jsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
-import styles from './ImageContainer.module.scss'
 import GenericContainer from '../generic-container/GenericContainer'
 import NameContainer from '../name-container/NameContainer'
 import SmallIcon from '@/components/icons/small-icon/SmallIcon'
+import styles from './ImageContainer.module.scss'
+import iconStyles from '@/components/icons/small-icon/SmallIcon.module.scss'
 
 export default function ImageContainer({
   xmlns1,
@@ -28,14 +29,14 @@ export default function ImageContainer({
         xmlns={xmlns1}
         icon={icon1}
         viewBox={viewBox1}
-        className={iconBg1}
+        className={`${iconBg1} ${iconStyles.left}`}
         iconFill={iconFill1}
       />
       <SmallIcon
         xmlns={xmlns2}
         icon={icon2}
         viewBox={viewBox2}
-        className={iconBg2}
+        className={`${iconBg2} ${iconStyles.right}`}
         iconFill={iconFill2}
       />
       <div className={`${styles.pictureContainer} ${bg2}`}>
